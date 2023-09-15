bf575453dd0f96ec6e11805bc1e4510b14eefb18
FkMooN <quoccao2001@gmail.com>
2023-09-15 18:19:21 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+
+const page = () => {
+  return (
+    <section className='w-full flex-col text-center'>
+      <h1 className='head_text text-center w-full'>
+        Discover and Share
+        <br className='max-md:hidden'/>
+        <span className='orange_gradient'>AI-Powered Prompts</span>
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+    </section>
+  )
+}
+
+export default page
